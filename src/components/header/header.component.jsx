99c0816a51505e463c0e843dbbe0755aa16d8686
Joke Heyndels <jo_hannah@outlook.com>
2020-04-15 20:12:52 +0200

    Update App.js and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,6 +18,7 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      <Link className="option" to="/sign-in">SIGN IN</Link>
     </div>
   </div>
 );
