414e9758a16e1421a6c6720112e5b1a82aeae06f
Joke Heyndels <jo_hannah@outlook.com>
2020-04-26 15:11:07 +0200

    Adjust header to check hidden value, toggle dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import { auth } from "../../firebase/firebase.utils"
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -28,12 +28,15 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
-    <CartDropdown />
+    {
+      hidden ? null : <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToPrups = state => ({
-  currentUser: state.user.currentUser
+const mapStateToPrups = ({ user: { currentUser }, cart: { hidden} }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToPrups)(Header);
