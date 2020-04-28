c214ee7d476a445cbdc4cb7b3e60278d504b181a
Joke Heyndels <jo_hannah@outlook.com>
2020-04-28 14:49:00 +0200

    Render empty message if user has no items in cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect"
+import { createStructuredSelector } from "reselect";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems } from "../../redux/cart/cart.selectors"
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {cartItems.map((cartItem) => (
-      <CartItem key={cartItem.id} item={cartItem} />
-    ))}
-    
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">No items in cart!</span>
+      )}
     </div>
     <CustomButton>Go to checkout</CustomButton>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
 });
 
 export default connect(mapStateToProps)(CartDropdown);
