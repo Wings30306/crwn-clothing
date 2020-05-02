fd946956e65e72f9e823e53be19aca1dabfcdd15
Joke Heyndels <jo_hannah@outlook.com>
2020-05-02 12:48:20 +0200

    Use styled-components in Header and HomePage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,56 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux"
-import { createStructuredSelector } from "reselect"
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import CartIcon from "../cart-icon/cart-icon.component"
-import CartDropdown from "../cart-dropdown/cart-dropdown.component"
-import { selectCartHidden } from "../../redux/cart/cart.selectors"
-import { selectCurrentUser } from "../../redux/user/user.selectors"
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-import { auth } from "../../firebase/firebase.utils"
+import { auth } from "../../firebase/firebase.utils";
 
-import "./header.styles.scss";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+  // OptionDiv,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className="logo" />
-    </Link>
+    </LogoContainer>
 
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
-      {
-        currentUser ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className="option" to="/sign-in">SIGN IN</Link>
-      }
+    <OptionsContainer>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/contact">CONTACT</OptionLink>
+      {currentUser ? (
+        <OptionLink as="div" onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+      ) : (
+        <OptionLink to="/sign-in">SIGN IN</OptionLink>
+      )}
       <CartIcon />
-    </div>
-    {
-      hidden ? null : <CartDropdown />
-    }
-  </div>
+    </OptionsContainer>
+    {hidden ? null : <CartDropdown />}
+  </HeaderContainer>
 );
 
 const mapStateToPrups = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
-})
+  hidden: selectCartHidden,
+});
 
 export default connect(mapStateToPrups)(Header);
 
-
 /**
  * NOTE FOR FUTURE REFERENCE:
- * 
+ *
  * import { ReactComponent as Logo }
- * 
+ *
  * This is a new special syntax when importing SVG in React. The ReactComponent import name is special and tells Create React App that you want a React component that renders an SVG, rather than its filename. You can read more about it here, but keep in mind that this is a React library special syntax:
- * 
+ *
  * https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files
  */
